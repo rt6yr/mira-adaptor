b402309430b64e071a1becb869155467efe6ad01
rt6yr <150139361+rt6yr@users.noreply.github.com>
2023-12-18 17:10:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import express from 'express';  
-
 import cors from 'cors';  
-import { createClient } from "@supabase/supabase-js";  
 import { ulid } from 'ulid';  
-
 import helmet from 'helmet';  
 import morgan from 'morgan';  
 import dotenv from "dotenv/config";  
