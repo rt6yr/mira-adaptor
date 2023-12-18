265d666fe4ae47c037ccfc2ceb1fe5bf204f0ad6
rt6yr <150139361+rt6yr@users.noreply.github.com>
2023-12-18 18:02:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import mainRoutes from './routes/main';
+import mainRoutes from '../routes/main';
 
 dotenv.config();
 
