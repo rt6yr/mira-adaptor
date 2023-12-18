b1e40f7cfd823215d295d11382b519afa48d428c
rt6yr <150139361+rt6yr@users.noreply.github.com>
2023-12-18 23:00:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import ejs from 'ejs';
+import path  from 'path';
 import mainRoutes from './routes/main.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+const landingEjsPath = path.join(__dirname, '../views/landingpage.ejs');
 
 app.use(cors());
 app.use(bodyParser.json());
